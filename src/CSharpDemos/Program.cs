fffb6ad2ce05d27da07d7dbcccd108eb6bc90f76
DannyAllegrezza <danny@dannyallegrezza.com>
2019-08-10 11:53:59 -0400

    Initial commit 🎉
    Stub out basic project

---
+++ src/CSharpDemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
